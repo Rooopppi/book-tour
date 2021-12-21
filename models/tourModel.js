63a6dd68240cdf3198955d9a1fb93aad815c6926
Rooopppi <denisrooopppi@gmail.com>
2021-12-21 18:42:00 +0200

    error handling

--- models/tourModel.js
+++ models/tourModel.js
@@ -107,17 +107,17 @@ tourSchema.pre('save', function(next) {
 
 // QUERY MIDDLEWARE
 // tourSchema.pre('find', function(next) {
-tourSchema.pre(/^find/, function(next) {
-  this.find({ secretTour: { $ne: true } });
+// tourSchema.pre(/^find/, function(next) {
+//   this.find({ secretTour: { $ne: true } });
 
-  this.start = Date.now();
-  next();
-});
+//   this.start = Date.now();
+//   next();
+// });
 
-tourSchema.post(/^find/, function(docs, next) {
-  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
-  next();
-});
+// tourSchema.post(/^find/, function(docs, next) {
+//   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+//   next();
+// });
 
 // AGGREGATION MIDDLEWARE
 tourSchema.pre('aggregate', function(next) {
